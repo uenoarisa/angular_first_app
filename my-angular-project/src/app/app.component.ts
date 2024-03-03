c33d06b221ad67cc6bdb7accd5dbdddee658a076
uenoarisa <arisa-choco0326@docomo.ne.jp>
2024-03-03 16:20:03 +0900

    angular tutorial終了

--- my-angular-project/src/app/app.component.ts
+++ my-angular-project/src/app/app.component.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
+    RouterModule,
   ],
   template: `
   <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.png" alt="logo" aria-hidden="true">
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
 `,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  
   title = 'homes';
   
 }
--- my-angular-project/src/app/home/home.component.ts
+++ my-angular-project/src/app/home/home.component.ts
@@ -1,19 +1,50 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+import { DetailsComponent } from '../details/details.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    DetailsComponent,
+    HousingLocationComponent
+  ],
   template: `
   <section>
-  <form>
-    <input type="text" placeholder="Filter by city">
-    <button class="primary" type="button">Search</button>
-  </form>
-</section>
+    <form>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="" type="button" (click)="filterResults(filter.value)">Search</button>
+    </form>
+  </section>
+  <section class="results">
+  <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
+  </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }  
 
 }
